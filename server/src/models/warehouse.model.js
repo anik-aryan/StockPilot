dda1bea918cddbab78e3ef340f7e5944c0a7c3c7
anik-aryan <anikaryan476@gmail.com>
2026-07-10 18:43:04 +0530

    add warehouse model

---
+++ server/src/models/warehouse.model.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const warehouseSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    code: {
+      type: String,
+      required: true,
+      uppercase: true,
+      unique: true,
+      trim: true,
+    },
+
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    state: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Warehouse", warehouseSchema);
