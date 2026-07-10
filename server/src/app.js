02ca45af53ef61a2cc1d89d2480a5c78257e9ed3
anik-aryan <anikaryan476@gmail.com>
2026-07-10 17:42:30 +0530

    Setup express server and health endpoint

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+
+app.get("/api/v1/health", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "StockPilot API is running successfully",
+    });
+});
+
+module.exports = app;
