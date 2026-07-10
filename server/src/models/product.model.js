a07233c77a77828eab17a57c88d5b08348205cf3
anik-aryan <anikaryan476@gmail.com>
2026-07-10 18:45:25 +0530

    added product model

---
+++ server/src/models/product.model.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    sku: {
+      type: String,
+      required: true,
+      unique: true,
+      uppercase: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    unit: {
+      type: String,
+      required: true,
+      enum: ["pcs", "kg", "litre", "box", "meter"],
+    },
+
+    reorderLevel: {
+      type: Number,
+      required: true,
+      default: 10,
+      min: 0,
+    },
+
+    costPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    sellingPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
