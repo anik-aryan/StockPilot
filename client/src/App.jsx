4b8287247a99cab562010e277f1653d5fedbb338
anik-aryan <anikaryan476@gmail.com>
2026-07-11 18:39:47 +0530

    Frontend setup using vite

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-emerald-600">
+        StockPilot
+      </h1>
+    </div>
+  );
+}
+
+export default App;
